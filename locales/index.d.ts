8de3b52a5fdde440fbe61ce2914217a747dd1767
1673beta <rekishinomori@gmail.com>
2024-03-24 21:24:25 +0900

    feat: Files Only Search Option

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -7328,6 +7328,10 @@ export interface Locale extends ILocale {
              * ノート検索の利用
              */
             "canSearchNotes": string;
+						/**
+						 * 高度な検索
+						 */
+						"canAdvancedSearchNotes": string;
             /**
              * 翻訳機能の利用
              */
@@ -11026,6 +11030,22 @@ export interface Locale extends ILocale {
          */
         "noResizeCompressLossy": string;
     };
+		"_advancedSearch": {
+			"_fileOption": {
+				/**
+				 * ファイル付きのみ
+				 */
+				"fileAttachedOnly": string;
+				/**
+				 * ファイルなし
+				 */
+				"noFile": string;
+				/**
+				 * どっちも
+				 */
+				"combined": string;
+			};
+		};
 }
 declare const locales: {
     [lang: string]: Locale;
