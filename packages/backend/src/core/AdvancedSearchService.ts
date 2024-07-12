6db10198356671fca5d898b54f315bc8a1f9c068
pen <121443048+penginn-net@users.noreply.github.com>
2024-07-12 20:03:40 +0000

    あった方がよさそう？

--- packages/backend/src/core/AdvancedSearchService.ts
+++ packages/backend/src/core/AdvancedSearchService.ts
@@ -303,16 +303,30 @@ export class AdvancedSearchService {
 			}
 
 			if (q !== '') {
-				osFilter.bool.must.push({
-					bool: {
-						should: [
-							{ wildcard: { 'text': { value: q } } },
-							{ simple_query_string: { fields: ['text'], 'query': q, default_operator: 'and' } },								{ wildcard: { 'cw': { value: q } } },
-							{ simple_query_string: { fields: ['cw'], 'query': q, default_operator: 'and' } },
-						],
-						minimum_should_match: 1,
-					} } );
-			}　
+				if (opts.excludeCW) {
+					osFilter.bool.must.push({
+						bool: {
+							should: [
+								{ wildcard: { 'text': { value: q } } },
+								{ simple_query_string: { fields: ['text'], 'query': q, default_operator: 'and' } },
+							],
+							minimum_should_match: 1,
+						},
+					});
+				} else {
+					osFilter.bool.must.push({
+						bool: {
+							should: [
+								{ wildcard: { 'text': { value: q } } },
+								{ simple_query_string: { fields: ['text'], 'query': q, default_operator: 'and' } },
+								{ wildcard: { 'cw': { value: q } } },
+								{ simple_query_string: { fields: ['cw'], 'query': q, default_operator: 'and' } },
+							],
+							minimum_should_match: 1,
+						},
+					});
+				}
+			}
 
 			const res = await this.opensearch.search({
 				index: this.opensearchNoteIndex as string,
