17635a3c399dae15095f0df56fd6fb42bb922ac0
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-07-13 16:15:05 +0900

    wip

--- packages/backend/src/server/api/endpoints/clips/show.ts
+++ packages/backend/src/server/api/endpoints/clips/show.ts
@@ -4,10 +4,13 @@
  */
 
 import { Inject, Injectable } from '@nestjs/common';
+import got, * as Got from 'got';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import type { ClipsRepository } from '@/models/_.js';
 import { ClipEntityService } from '@/core/entities/ClipEntityService.js';
 import { DI } from '@/di-symbols.js';
+import type { Config } from '@/config.js';
+import { HttpRequestService } from '@/core/HttpRequestService.js';
 import { ApiError } from '../../error.js';
 
 export const meta = {
@@ -23,6 +26,11 @@ export const meta = {
 			code: 'NO_SUCH_CLIP',
 			id: 'c3c5fe33-d62c-44d2-9ea5-d997703f5c20',
 		},
+		invalidIdFormat: {
+			message: 'Invalid id format.',
+			code: 'INVALID_ID_FORMAT',
+			id: 'df45c7d1-cd15-4a35-b3e1-8c9f987c4f5c',
+		},
 	},
 
 	res: {
@@ -35,7 +43,7 @@ export const meta = {
 export const paramDef = {
 	type: 'object',
 	properties: {
-		clipId: { type: 'string', format: 'misskey:id' },
+		clipId: { type: 'string' },
 	},
 	required: ['clipId'],
 } as const;
@@ -43,12 +51,24 @@ export const paramDef = {
 @Injectable()
 export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-disable-line import/no-default-export
 	constructor(
+		@Inject(DI.config)
+		private config: Config,
 		@Inject(DI.clipsRepository)
 		private clipsRepository: ClipsRepository,
 
+		private httpRequestService: HttpRequestService,
 		private clipEntityService: ClipEntityService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
+			const parsed_id = ps.clipId.split('@');
+			if (parsed_id.length === 2 ) {//is remote
+				const url = 'https://' + parsed_id[1] + '/clips/' + parsed_id[0];
+				console.log(url);
+				throw new ApiError(meta.errors.invalidIdFormat);
+			}
+			if (parsed_id.length !== 1 ) {//is not local
+				throw new ApiError(meta.errors.invalidIdFormat);
+			}
 			// Fetch the clip
 			const clip = await this.clipsRepository.findOneBy({
 				id: ps.clipId,
@@ -65,4 +85,32 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			return await this.clipEntityService.pack(clip, me);
 		});
 	}
+}
+
+async function remote(config:Config, httpRequestService: HttpRequestService, url:string) {
+	const timeout = 30 * 1000;
+	const operationTimeout = 60 * 1000;
+	const req = got.stream(url, {
+		headers: {
+			'User-Agent': config.userAgent,
+		},
+		timeout: {
+			lookup: timeout,
+			connect: timeout,
+			secureConnect: timeout,
+			socket: timeout,	// read timeout
+			response: timeout,
+			send: timeout,
+			request: operationTimeout,	// whole operation timeout
+		},
+		agent: {
+			http: httpRequestService.httpAgent,
+			https: httpRequestService.httpsAgent,
+		},
+		http2: true,
+		retry: {
+			limit: 1,
+		},
+		enableUnixSockets: false,
+	});
 }
