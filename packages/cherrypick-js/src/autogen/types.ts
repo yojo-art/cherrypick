b591c634fd06980bb47b193f6ec1275998ba5215
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-07-14 03:36:57 +0900

    pnpm run build-cherrypick-js-with-types

--- packages/cherrypick-js/src/autogen/types.ts
+++ packages/cherrypick-js/src/autogen/types.ts
@@ -13291,7 +13291,6 @@ export type operations = {
     requestBody: {
       content: {
         'application/json': {
-          /** Format: misskey:id */
           clipId: string;
           /** @default 10 */
           limit?: number;
@@ -13461,7 +13460,6 @@ export type operations = {
     requestBody: {
       content: {
         'application/json': {
-          /** Format: misskey:id */
           clipId: string;
         };
       };
@@ -25941,9 +25939,7 @@ export type operations = {
           userId: string;
           /** @default 10 */
           limit?: number;
-          /** Format: misskey:id */
           sinceId?: string;
-          /** Format: misskey:id */
           untilId?: string;
         };
       };
