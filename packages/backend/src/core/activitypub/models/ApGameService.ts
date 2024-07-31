0b7bf5170ef57c592a5818d9bdbeb6c217f3808a
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-07-31 11:49:54 +0900

    wip

--- packages/backend/src/core/activitypub/models/ApGameService.ts
+++ packages/backend/src/core/activitypub/models/ApGameService.ts
@@ -9,6 +9,7 @@ import { bindThis } from '@/decorators.js';
 import type { MiRemoteUser, MiUser } from '@/models/User.js';
 import type { Config } from '@/config.js';
 import { DI } from '@/di-symbols.js';
+import { NotificationService } from '@/core/NotificationService.js';
 import { isGame } from '../type.js';
 import { ApLoggerService } from '../ApLoggerService.js';
 import { ApResolverService } from '../ApResolverService.js';
@@ -18,10 +19,6 @@ import type { IApGame, ICreate, IInvite, IObject } from '../type.js';
 
 @Injectable()
 export class ApGameService {
-	reversiInboxInvite(local_user: MiUser, remote_user: MiUser, game_state: any) {
-		console.log(remote_user);
-		console.log(local_user);
-	}
 	private logger: Logger;
 
 	constructor(
@@ -30,19 +27,30 @@ export class ApGameService {
 
 		private apResolverService: ApResolverService,
 		private userEntityService: UserEntityService,
+		private notificationService: NotificationService,
 		private apLoggerService: ApLoggerService,
 	) {
 		this.logger = this.apLoggerService.logger;
 	}
+	reversiInboxInvite(local_user: MiUser, remote_user: MiUser, game_state: any) {
+		console.log(remote_user);
+		console.log(local_user);
+		this.notificationService.createNotification(local_user.id, 'app', {
+			customBody: 'reversiInboxInvite',
+			customHeader: null,
+			customIcon: null,
+			appAccessTokenId: null,
+		});
+	}
 	@bindThis
-	public async renderReversiInvite(invite_id:string, invite_from:MiUser, invite_to:MiRemoteUser, invite_date:Date): Promise<IInvite> {
+	public async renderReversiInvite(game_session_id:string, invite_from:MiUser, invite_to:MiRemoteUser, invite_date:Date): Promise<IInvite> {
 		const game:IApGame = {
 			type: 'Game',
 			game_type_uuid: '1c086295-25e3-4b82-b31e-3e3959906312',
 			game_state: null,
 		};
 		const activity: IInvite = {
-			id: `${this.config.url}/games/${game.game_type_uuid}/${invite_id}/activity`,
+			id: `${this.config.url}/games/${game.game_type_uuid}/${game_session_id}/activity`,
 			actor: this.userEntityService.genLocalUserUri(invite_from.id),
 			type: 'Invite',
 			published: invite_date.toISOString(),
