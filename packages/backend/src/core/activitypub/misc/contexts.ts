9bf9f4b4259d7733ead024ef8cdd40a0bbdce7a5
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-08-15 10:35:00 +0900

    contexts.tsにyojoart:bannerを定義

--- packages/backend/src/core/activitypub/misc/contexts.ts
+++ packages/backend/src/core/activitypub/misc/contexts.ts
@@ -556,6 +556,9 @@ const extension_context_definition = {
 	'_misskey_summary': 'misskey:_misskey_summary',
 	'_misskey_talk': 'misskey:_misskey_talk',
 	'isCat': 'misskey:isCat',
+	// yojo-art
+	yojoart: 'https://yojoart.kzkr.xyz/ns#',
+	'banner': 'yojoart:banner',
 	// vcard
 	vcard: 'http://www.w3.org/2006/vcard/ns#',
 } satisfies Context;
