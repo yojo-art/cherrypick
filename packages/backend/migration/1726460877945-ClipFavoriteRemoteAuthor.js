05aa457e0e64575fd210e7cefa412f6d900f072c
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-09-16 14:08:34 +0900

    fix

--- packages/backend/migration/1726460877945-ClipFavoriteRemoteAuthor.js
+++ packages/backend/migration/1726460877945-ClipFavoriteRemoteAuthor.js
@@ -7,10 +7,10 @@ export class clipFavoriteRemoteAuthor1726460877945 {
 	name = 'clipFavoriteRemoteAuthor1726460877945'
 
 	async up(queryRunner) {
-			await queryRunner.query(`ALTER TABLE "clip_like_remote" ADD "authorId" character varying(32)`);
+			await queryRunner.query(`ALTER TABLE "clip_favorite_remote" ADD "authorId" character varying(32)`);
 	}
 
 	async down(queryRunner) {
-			await queryRunner.query(`ALTER TABLE "clip_like_remote" DROP COLUMN "authorId"`);
+			await queryRunner.query(`ALTER TABLE "clip_favorite_remote" DROP COLUMN "authorId"`);
 	}
 }
