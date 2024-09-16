bf665ebe766a4f0cb224e8209d2da6961d4d028a
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-09-16 18:36:24 +0900

    CONSTRAINT

--- packages/backend/migration/1726460877945-ClipFavoriteRemoteAuthor.js
+++ packages/backend/migration/1726460877945-ClipFavoriteRemoteAuthor.js
@@ -8,9 +8,11 @@ export class clipFavoriteRemoteAuthor1726460877945 {
 
 	async up(queryRunner) {
 			await queryRunner.query(`ALTER TABLE "clip_favorite_remote" ADD "authorId" character varying(32)`);
+			await queryRunner.query(`ALTER TABLE "clip_favorite_remote" ADD CONSTRAINT "FK_e306e7566fd6101e9767702980c" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
 	}
 
 	async down(queryRunner) {
+		await queryRunner.query(`ALTER TABLE "clip_favorite_remote" DROP CONSTRAINT "FK_e306e7566fd6101e9767702980c"`);
 			await queryRunner.query(`ALTER TABLE "clip_favorite_remote" DROP COLUMN "authorId"`);
 	}
 }
