399f63c606428a4dc1e128b207baa0bb7c07fe75
penginn-net <121443048+penginn-net@users.noreply.github.com>
2024-09-27 07:00:07 +0900

    fix

--- packages/backend/src/core/ReactionService.ts
+++ packages/backend/src/core/ReactionService.ts
@@ -308,7 +308,7 @@ export class ReactionService {
 
 		// Delete reaction
 		const result = await this.noteReactionsRepository.delete(exist.id);
-		await this.advancedSearchService.unindexReaction(exist.id);
+		await this.advancedSearchService.unindexReaction(exist.id, user.host === null ? false : true);
 
 		if (result.affected !== 1) {
 			throw new IdentifiableError('60527ec9-b4cb-4a88-a6bd-32d3ad26817d', 'not reacted');
