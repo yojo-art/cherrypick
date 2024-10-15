d1e387f7a0d3c98a5bc8edd23fa02f2abdd0b712
penginn-net <121443048+penginn-net@users.noreply.github.com>
2024-10-15 21:08:03 +0900

    a

--- packages/backend/src/core/AdvancedSearchService.ts
+++ packages/backend/src/core/AdvancedSearchService.ts
@@ -918,9 +918,8 @@ export class AdvancedSearchService {
 		const FilterdNotes = [] as OpenSearchHit[];
 		while ( FilterdNotes.length < OpenSearchOption.size) {
 			const res = await this.opensearch.search(OpenSearchOption);
-			if (res.body.hits.hits.length === 0) break;//これ以上探してもない
 			notes = res.body.hits.hits as OpenSearchHit[];
-
+			if (notes.length === 0) break;//これ以上探してもない
 			const resultPromises = notes.map(x => this.filter(x, Filter, Followings, meUserId));
 			const Results = (await Promise.all(resultPromises)).filter( (x) => x !== null);
 			if (Results.length > 0) {
