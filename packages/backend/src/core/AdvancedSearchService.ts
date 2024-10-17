4d2d645a252c54dd23e816bfe617cb8dd6caeec7
penginn-net <121443048+penginn-net@users.noreply.github.com>
2024-10-17 19:36:58 +0900

    fix

--- packages/backend/src/core/AdvancedSearchService.ts
+++ packages/backend/src/core/AdvancedSearchService.ts
@@ -854,7 +854,7 @@ export class AdvancedSearchService {
 			}
 			if (opts.excludeReply) osFilter.bool.must_not.push({ exists: { field: 'replyId' } });
 			if (opts.excludeCW) osFilter.bool.must_not.push({ exists: { field: 'cw' } });
-			if (opts.excludeQuote) osFilter.bool.must.push({ term: { field: 'renoteId' } });
+			if (opts.excludeQuote) osFilter.bool.must.push({ exists: { field: 'renoteId' } });
 			if (opts.fileOption) {
 				if (opts.fileOption === 'file-only') {
 					osFilter.bool.must.push({ exists: { field: 'fileIds' } });
