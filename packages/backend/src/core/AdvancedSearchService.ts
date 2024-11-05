c647faa7f4339b541653fbf222b3f4825dd61ba8
pen <121443048+penginn-net@users.noreply.github.com>
2024-11-05 13:54:53 +0900

    Fix: 高度な検索でノート本文に含まれないタグが検索対象外なのを修正 (#530)

--- packages/backend/src/core/AdvancedSearchService.ts
+++ packages/backend/src/core/AdvancedSearchService.ts
@@ -956,7 +956,7 @@ export class AdvancedSearchService {
 			}
 
 			if (q && q !== '') {
-				const fields = [opts.useStrictSearch ? 'text.keyword' : 'text'];
+				const fields = ['tags', opts.useStrictSearch ? 'text.keyword' : 'text'];
 				if (!opts.excludeCW) {	fields.push(opts.useStrictSearch ? 'cw.keyword' : 'cw' );}
 				osFilter.bool.must.push({ simple_query_string: { fields: fields, 'query': q, default_operator: 'and' } });
 			}
