fd7e33e09f5cd7547a7eb4e85e557fcdf1de943c
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-18 19:52:25 +0900

    log

--- packages/backend/src/core/QueueService.ts
+++ packages/backend/src/core/QueueService.ts
@@ -118,6 +118,8 @@ export class QueueService {
 			}
 		}
 		if (toPublicOnly) {
+			console.log('to=' + content.to);
+			console.log('cc=' + content.cc);
 			return content.to === 'https://www.w3.org/ns/activitystreams#Public' || content.cc === 'https://www.w3.org/ns/activitystreams#Public';
 		} else {
 			return true;
