6e70d8c9fdb4dff54760f8733fdd563e168201c1
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-26 19:58:07 +0900

    rename

--- packages/frontend-embed/src/boot.ts
+++ packages/frontend-embed/src/boot.ts
@@ -23,7 +23,7 @@ import { serverMetadata } from '@/server-metadata.js';
 import { postMessageToParentWindow, setIframeId } from '@/post-message.js';
 import { serverContext } from '@/server-context.js';
 
-console.log('YojoArt Embed');
+console.log('yojo-art Embed');
 
 //#region Embedパラメータの取得・パース
 const params = new URLSearchParams(location.search);
