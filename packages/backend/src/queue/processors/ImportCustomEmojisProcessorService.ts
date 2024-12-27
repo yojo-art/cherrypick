be05e441a4fb4f9ef9be0a3e35e6066f61b9cf2b
penginn-net <121443048+penginn-net@users.noreply.github.com>
2024-12-27 15:03:55 +0000

    lint

--- packages/backend/src/queue/processors/ImportCustomEmojisProcessorService.ts
+++ packages/backend/src/queue/processors/ImportCustomEmojisProcessorService.ts
@@ -103,6 +103,10 @@ export class ImportCustomEmojisProcessorService {
 						license: emojiInfo.license,
 						isSensitive: emojiInfo.isSensitive,
 						localOnly: emojiInfo.localOnly,
+						author: emojiInfo.author ?? null,
+						description: emojiInfo.description ?? null,
+						usageInfo: emojiInfo.usageInfo ?? null,
+						copyPermission: emojiInfo.copyPermission ?? null,
 						roleIdsThatCanBeUsedThisEmojiAsReaction: [],
 					});
 				} catch (e) {
