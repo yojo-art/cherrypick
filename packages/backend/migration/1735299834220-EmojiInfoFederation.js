895af357c2f790930321f18f17b2bfcf23497a64
penginn-net <121443048+penginn-net@users.noreply.github.com>
2025-01-05 14:05:04 +0000

    fix migration

--- packages/backend/migration/1735299834220-EmojiInfoFederation.js
+++ packages/backend/migration/1735299834220-EmojiInfoFederation.js
@@ -8,6 +8,7 @@ export class EmojiInfoFederation1735299834220 {
 			await queryRunner.query(`ALTER TABLE "emoji" ADD "usageInfo" character varying(512)`);
 			await queryRunner.query(`ALTER TABLE "emoji" ADD "description" character varying(512)`);
 			await queryRunner.query(`ALTER TABLE "emoji" ADD "author" character varying(128)`);
+			await queryRunner.query(`ALTER TABLE "emoji" ADD "isBasedOn" character varying(1024)`);
 			await queryRunner.query(`CREATE TYPE "public"."emoji_copypermission_enum" AS ENUM('allow', 'deny', 'conditional', 'null')`);
 			await queryRunner.query(`ALTER TABLE "emoji" ADD "copyPermission" "public"."emoji_copypermission_enum"`);
     }
@@ -15,6 +16,7 @@ export class EmojiInfoFederation1735299834220 {
     async down(queryRunner) {
 			await queryRunner.query(`ALTER TABLE "emoji" DROP COLUMN "copyPermission"`);
 			await queryRunner.query(`DROP TYPE "public"."emoji_copypermission_enum"`);
+			await queryRunner.query(`ALTER TABLE "emoji" DROP COLUMN "isBasedOn"`);
 			await queryRunner.query(`ALTER TABLE "emoji" DROP COLUMN "author"`);
 			await queryRunner.query(`ALTER TABLE "emoji" DROP COLUMN "description"`);
 			await queryRunner.query(`ALTER TABLE "emoji" DROP COLUMN "usageInfo"`);
