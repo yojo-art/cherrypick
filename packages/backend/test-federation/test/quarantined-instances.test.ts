453e5cbeadd54c134292860e92dcc338e9ee8b61
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-01-11 11:40:05 +0900

    wip

--- packages/backend/test-federation/test/quarantined-instances.test.ts
+++ packages/backend/test-federation/test/quarantined-instances.test.ts
@@ -4,7 +4,7 @@ import { createAccount, fetchAdmin, type LoginUser, resolveRemoteUser, sleep } f
 
 describe('quarantine instance', () => {
 	let alice: LoginUser, bob: LoginUser, carol:LoginUser;
-	let aliceInBobHost: Misskey.entities.UserDetailedNotMe, bobInBobHost: Misskey.entities.UserDetailedNotMe, carolInBobHost: Misskey.entities.UserDetailedNotMe;
+	let aliceInBobHost: Misskey.entities.UserDetailedNotMe, bobInAliceHost: Misskey.entities.UserDetailedNotMe, carolInBobHost: Misskey.entities.UserDetailedNotMe;
 	let aAdmin: LoginUser;
 
 	beforeAll(async () => {
@@ -14,7 +14,7 @@ describe('quarantine instance', () => {
 			createAccount('a.test'),
 		]);
 
-		[aliceInBobHost, bobInBobHost, carolInBobHost] = await Promise.all([
+		[aliceInBobHost, bobInAliceHost, carolInBobHost] = await Promise.all([
 			resolveRemoteUser('a.test', alice.id, bob),
 			resolveRemoteUser('b.test', bob.id, alice),
 			resolveRemoteUser('a.test', carol.id, bob),
@@ -23,97 +23,165 @@ describe('quarantine instance', () => {
 		aAdmin = await fetchAdmin('a.test');
 		await sleep();
 	});
-	test('isQuarantineLimit true', async () => {
+	describe('isQuarantineLimit true', async () => {
 		await aAdmin.client.request('admin/federation/update-instance', { host: 'a.test', isQuarantineLimit: true });
-
 		const alicePublicNote: Misskey.entities.Note = (await alice.client.request('notes/create', { text: 'I am Alice!' })).createdNote;
 		const alicePublicRenote: Misskey.entities.Note = (await alice.client.request('notes/create', { renoteId: alicePublicNote.id })).createdNote;
+		const expected :{text:string|null, createdAt:string}[] = [];
+		expected.push({
+			text: alicePublicNote.text,
+			createdAt: alicePublicNote.createdAt,
+		});
+		expected.push({
+			text: alicePublicRenote.text,
+			createdAt: alicePublicRenote.createdAt,
+		});
+		await sleep();
+		test('public', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: aliceInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 		const aliceHomeNote: Misskey.entities.Note = (await alice.client.request('notes/create', { text: 'home note', visibility: 'home' })).createdNote;
 		const aliceHomeRenote: Misskey.entities.Note = (await alice.client.request('notes/create', { renoteId: alicePublicNote.id, visibility: 'home' })).createdNote;
+		expected.push({
+			text: aliceHomeNote.text,
+			createdAt: aliceHomeNote.createdAt,
+		});
+		expected.push({
+			text: aliceHomeRenote.text,
+			createdAt: aliceHomeRenote.createdAt,
+		});
+		await sleep();
+		test('home', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: aliceInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 		(await alice.client.request('notes/create', { text: 'followers note', visibility: 'followers' })).createdNote;
 		(await alice.client.request('notes/create', { renoteId: alicePublicNote.id, visibility: 'followers' })).createdNote;
-		(await alice.client.request('notes/create', { text: 'specified note', visibility: 'specified', visibleUserIds: [bobInBobHost.id] })).createdNote;
-		(await alice.client.request('notes/create', { renoteId: alicePublicNote.id, visibility: 'specified', visibleUserIds: [bobInBobHost.id] })).createdNote;
-
 		await sleep();
-		const fetch_notes = await bob.client.request('users/notes', { userId: aliceInBobHost.id, withReplies: false, withRenotes: true });
-		strictEqual(fetch_notes.length, 4, JSON.stringify(fetch_notes));
-		deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
-			return {
-				text: note.text,
-				createdAt: note.createdAt,
-			};
-		})), JSON.stringify([
-			{
-				text: aliceHomeRenote.text,
-				createdAt: aliceHomeRenote.createdAt,
-			}, {
-				text: aliceHomeNote.text,
-				createdAt: aliceHomeNote.createdAt,
-			},
-			{
-				text: alicePublicRenote.text,
-				createdAt: alicePublicRenote.createdAt,
-			}, {
-				text: alicePublicNote.text,
-				createdAt: alicePublicNote.createdAt,
-			},
-		]));
-		strictEqual(fetch_notes[0].renote?.id, fetch_notes[1].id);
+		test('followers', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: aliceInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
+		(await alice.client.request('notes/create', { text: 'specified note', visibility: 'specified', visibleUserIds: [bobInAliceHost.id] })).createdNote;
+		(await alice.client.request('notes/create', { renoteId: alicePublicNote.id, visibility: 'specified', visibleUserIds: [bobInAliceHost.id] })).createdNote;
+		await sleep();
+		test('specified', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: aliceInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 	});
 	test('isQuarantineLimit false', async () => {
 		await aAdmin.client.request('admin/federation/update-instance', { host: 'a.test', isQuarantineLimit: false });
 
 		const carolPublicNote: Misskey.entities.Note = (await carol.client.request('notes/create', { text: 'I am Carol!' })).createdNote;
 		const carolPublicRenote: Misskey.entities.Note = (await carol.client.request('notes/create', { renoteId: carolPublicNote.id })).createdNote;
+		const expected :{text:string|null, createdAt:string}[] = [];
+		expected.push({
+			text: carolPublicNote.text,
+			createdAt: carolPublicNote.createdAt,
+		});
+		expected.push({
+			text: carolPublicRenote.text,
+			createdAt: carolPublicRenote.createdAt,
+		});
+		await sleep();
+		test('public', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: carolInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 		const carolHomeNote: Misskey.entities.Note = (await carol.client.request('notes/create', { text: 'home note', visibility: 'home' })).createdNote;
 		const carolHomeRenote: Misskey.entities.Note = (await carol.client.request('notes/create', { renoteId: carolPublicNote.id, visibility: 'home' })).createdNote;
+		expected.push({
+			text: carolHomeNote.text,
+			createdAt: carolHomeNote.createdAt,
+		});
+		expected.push({
+			text: carolHomeRenote.text,
+			createdAt: carolHomeRenote.createdAt,
+		});
+		await sleep();
+		test('home', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: carolInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 		const carolFollowersNote: Misskey.entities.Note = (await carol.client.request('notes/create', { text: 'followers note', visibility: 'followers' })).createdNote;
 		const carolFollowersRenote: Misskey.entities.Note = (await carol.client.request('notes/create', { renoteId: carolPublicNote.id, visibility: 'followers' })).createdNote;
-		const carolSpecifiedNote: Misskey.entities.Note = (await carol.client.request('notes/create', { text: 'specified note', visibility: 'specified', visibleUserIds: [bobInBobHost.id] })).createdNote;
-		const carolSpecifiedRenote: Misskey.entities.Note = (await carol.client.request('notes/create', { renoteId: carolPublicNote.id, visibility: 'specified', visibleUserIds: [bobInBobHost.id] })).createdNote;
-
+		expected.push({
+			text: carolFollowersNote.text,
+			createdAt: carolFollowersNote.createdAt,
+		});
+		expected.push({
+			text: carolFollowersRenote.text,
+			createdAt: carolFollowersRenote.createdAt,
+		});
 		await sleep();
-		const fetch_notes = await bob.client.request('users/notes', { userId: carolInBobHost.id, withReplies: false, withRenotes: true });
-		strictEqual(fetch_notes.length, 8, JSON.stringify(fetch_notes));
-		deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
-			return {
-				text: note.text,
-				createdAt: note.createdAt,
-			};
-		})), JSON.stringify([
-			{
-				text: carolSpecifiedRenote.text,
-				createdAt: carolSpecifiedRenote.createdAt,
-			}, {
-				text: carolSpecifiedNote.text,
-				createdAt: carolSpecifiedNote.createdAt,
-			},
-			{
-				text: carolFollowersRenote.text,
-				createdAt: carolFollowersRenote.createdAt,
-			}, {
-				text: carolFollowersNote.text,
-				createdAt: carolFollowersNote.createdAt,
-			},
-			{
-				text: carolHomeRenote.text,
-				createdAt: carolHomeRenote.createdAt,
-			}, {
-				text: carolHomeNote.text,
-				createdAt: carolHomeNote.createdAt,
-			},
-			{
-				text: carolPublicRenote.text,
-				createdAt: carolPublicRenote.createdAt,
-			}, {
-				text: carolPublicNote.text,
-				createdAt: carolPublicNote.createdAt,
-			},
-		]));
-		strictEqual(fetch_notes[0].renote?.id, fetch_notes[7].id);
-		strictEqual(fetch_notes[2].renote?.id, fetch_notes[7].id);
-		strictEqual(fetch_notes[4].renote?.id, fetch_notes[7].id);
-		strictEqual(fetch_notes[6].renote?.id, fetch_notes[7].id);
+		test('followers', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: carolInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
+		const carolSpecifiedNote: Misskey.entities.Note = (await carol.client.request('notes/create', { text: 'specified note', visibility: 'specified', visibleUserIds: [bobInAliceHost.id] })).createdNote;
+		const carolSpecifiedRenote: Misskey.entities.Note = (await carol.client.request('notes/create', { renoteId: carolPublicNote.id, visibility: 'specified', visibleUserIds: [bobInAliceHost.id] })).createdNote;
+		expected.push({
+			text: carolSpecifiedNote.text,
+			createdAt: carolSpecifiedNote.createdAt,
+		});
+		expected.push({
+			text: carolSpecifiedRenote.text,
+			createdAt: carolSpecifiedRenote.createdAt,
+		});
+		await sleep();
+		test('followers', async () => {
+			const fetch_notes = await bob.client.request('users/notes', { userId: carolInBobHost.id, withReplies: false, withRenotes: true });
+			strictEqual(fetch_notes.length, expected.length, JSON.stringify(fetch_notes));
+			deepStrictEqual(JSON.stringify(fetch_notes.map(note => {
+				return {
+					text: note.text,
+					createdAt: note.createdAt,
+				};
+			})), JSON.stringify(Array.from(expected).reverse()));
+		});
 	});
 });
