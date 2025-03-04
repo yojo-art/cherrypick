1a0337d57e09e1adebb3ac560baa42d9bf887d93
penginn-net <121443048+penginn-net@users.noreply.github.com>
2025-03-04 12:22:24 +0000

    fix

--- packages/backend/src/server/api/endpoints/federation/remote-software.ts
+++ packages/backend/src/server/api/endpoints/federation/remote-software.ts
@@ -4,10 +4,10 @@
  */
 
 import { Inject, Injectable } from '@nestjs/common';
+import { MoreThan } from 'typeorm';
 import type { InstancesRepository } from '@/models/_.js';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { DI } from '@/di-symbols.js';
-import { MoreThan } from "typeorm";
 
 export const meta = {
 	tags: ['federation'],
@@ -40,8 +40,8 @@ export const meta = {
 					type: 'integer',
 					optional: false,
 					nullable: false,
-				}
-			}
+				},
+			},
 		},
 	},
 } as const;
@@ -52,8 +52,8 @@ export const paramDef = {
 		allInstance: {
 			type: 'boolean',
 			default: false,
-			description: '`true`にすると連合していないインスタンスも含めます'
-			},
+			description: '`true`にすると連合していないインスタンスも含めます',
+		},
 	},
 	required: [],
 } as const;
@@ -65,38 +65,34 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private instancesRepository: InstancesRepository,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-		const queryResult = ps.allInstance ? await this.instancesRepository.find({
+			const queryResult = ps.allInstance ? await this.instancesRepository.find({
 				select: ['softwareName'],
-			}
-		) : await this.instancesRepository.find({
-					where: [
-						{ followersCount: MoreThan(0) },
-						{ followingCount: MoreThan(0) },
-					],
-					select: ['softwareName'],
-				}
+			},
+			) : await this.instancesRepository.find({
+				where: [
+					{ followersCount: MoreThan(0) },
+					{ followingCount: MoreThan(0) },
+				],
+				select: ['softwareName'],
+			},
 			);
 
-			const softwareName = [ ...new Set (queryResult.map( x => x.softwareName))];
-			return  softwareName.map(s => {
-				return {
-					softwareName: s ?? 'null',
-					count: queryResult.filter(x => x.softwareName === s).length,
-					color: getColor(s)
-				};
-			});
+			const softwareName = [...new Set(queryResult.map( x => x.softwareName))];
+			return softwareName.map(s => ({
+				softwareName: s ?? 'null',
+				count: queryResult.filter(x => x.softwareName === s).length,
+				color: getColor(s),
+			}));
 		});
 	}
 }
 
-function getColor(name: string | null): string | null
-{
-	switch (name)
-	{
+function getColor(name: string | null): string | null {
+	switch (name) {
 		case 'misskey':
 			return '#86b300';
 		case 'sharkey':
-			return '#86b300';
+			return '#43BBE5';
 
 		case 'cherrypick':
 			return '#ffa9c3';
@@ -105,6 +101,8 @@ function getColor(name: string | null): string | null
 
 		case 'mastodon':
 			return '#6364FF';
+		case 'kmyblue':
+			return '#86AFE5';
 		case 'fedibird':
 			return '#282c37';
 
@@ -113,8 +111,8 @@ function getColor(name: string | null): string | null
 		case 'akkoma':
 			return '#462E7A';
 
-			//hollo,mitra,null
-			default:
-				return null;
+		//hollo,mitra,null
+		default:
+			return null;
 	}
 }
