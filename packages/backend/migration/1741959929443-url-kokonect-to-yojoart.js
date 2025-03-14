3b729c0772bded6c6764ab7af8cbe50b408e53a5
pen <121443048+penginn-net@users.noreply.github.com>
2025-03-14 23:07:53 +0900

    Update 1741959929443-url-kokonect-to-yojoart.js

--- packages/backend/migration/1741959929443-url-kokonect-to-yojoart.js
+++ packages/backend/migration/1741959929443-url-kokonect-to-yojoart.js
@@ -7,8 +7,8 @@ export class UrlKokonectToYojoart1741959929443 {
     name = 'UrlKokonectToYojoart1741959929443'
 
     async up(queryRunner) {
-        await queryRunner.query(`UPDATE "meta" SET "repositoryUrl" = 'https://github.com/yojo-art/cherrypick' WHERE "repositoryUrl" = ''https://github.com/kokonect-link/cherrypick'`);
-        await queryRunner.query(`UPDATE "meta" SET "feedbackUrl" = 'https://github.com/yojo-art/cherrypick' WHERE "feedbackUrl" = ''https://github.com/kokonect-link/cherrypick'`);
+        await queryRunner.query(`UPDATE "meta" SET "repositoryUrl" = 'https://github.com/yojo-art/cherrypick' WHERE "repositoryUrl" = 'https://github.com/kokonect-link/cherrypick'`);
+        await queryRunner.query(`UPDATE "meta" SET "feedbackUrl" = 'https://github.com/yojo-art/cherrypick' WHERE "feedbackUrl" = 'https://github.com/kokonect-link/cherrypick'`);
     }
 
     async down(queryRunner) {
