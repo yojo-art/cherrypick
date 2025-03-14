f7e9353365d744997423c12cfcd3107c79c808df
pen <121443048+penginn-net@users.noreply.github.com>
2025-03-14 22:59:40 +0900

    feedbackurl

--- packages/backend/migration/1741959929443-url-kokonect-to-yojoart.js
+++ packages/backend/migration/1741959929443-url-kokonect-to-yojoart.js
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export class RepositoryUrlKokonectToYojoart1741959929443 {
-    name = 'RepositoryUrlKokonectToYojoart1741959929443'
+export class UrlKokonectToYojoart1741959929443 {
+    name = 'UrlKokonectToYojoart1741959929443'
 
     async up(queryRunner) {
         await queryRunner.query(`UPDATE "meta" SET "repositoryUrl" = 'https://github.com/yojo-art/cherrypick' WHERE "repositoryUrl" = ''https://github.com/kokonect-link/cherrypick'`);
+        await queryRunner.query(`UPDATE "meta" SET "feedbackUrl" = 'https://github.com/yojo-art/cherrypick/issues/new' WHERE "repositoryUrl" = ''https://github.com/kokonect-link/cherrypick/issues/new'`);
     }
 
     async down(queryRunner) {
