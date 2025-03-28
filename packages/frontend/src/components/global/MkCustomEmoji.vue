5f854efd927c0905b61d96dd39cb990640225b05
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-03-28 14:41:10 +0900

    ノート内絵文字を付ける機能でローカルに無い絵文字は機能を非表示に (#683)

--- packages/frontend/src/components/global/MkCustomEmoji.vue
+++ packages/frontend/src/components/global/MkCustomEmoji.vue
@@ -121,13 +121,18 @@ function onClick(ev: MouseEvent) {
 		}
 
 		if (props.menuReaction && react) {
-			menuItems.push({
-				text: i18n.ts.doReaction,
-				icon: 'ti ti-mood-plus',
-				action: () => {
-					react(`:${props.name}:`);
-				},
-			});
+			if (props.host && !customEmojisMap.get(props.name)) {
+				//ローカルに絵文字が無い時メニューに追加しない
+				//https://github.com/yojo-art/cherrypick/pull/683
+			} else {
+				menuItems.push({
+					text: i18n.ts.doReaction,
+					icon: 'ti ti-mood-plus',
+					action: () => {
+						react(`:${props.name}:`);
+					},
+				});
+			}
 		}
 
 		menuItems.push({
