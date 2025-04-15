49198e1eaa0aafd0a4618da62008ea52fd3b45e0
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-04-15 22:27:48 +0900

    10s

--- packages/backend/test-federation/test/user.test.ts
+++ packages/backend/test-federation/test/user.test.ts
@@ -234,9 +234,9 @@ describe('User', () => {
 
 			await bob.client.request('users/show', { userId: aliceInB.id });
 			await bob.client.request('federation/update-remote-user', { userId: aliceInB.id });
-			await sleep(1000);
+			await sleep(10000);
 			await bob.client.request('users/clips', { userId: aliceInB.id, remoteApi: false });
-			await sleep(1000);
+			await sleep(10000);
 			const aliceInBClips = await bob.client.request('users/clips', { userId: aliceInB.id, remoteApi: false });
 			strictEqual(aliceInBClips.length, 1);
 			strictEqual(aliceInBClips[0].name, clip.name);
