35743e4d9de30c8b1fda32a7713e2b26d8994913
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-04-16 14:33:21 +0900

    Revert "見る用"
    This reverts commit 2a545a7b8fee0efcd04dadae81b16b455a78494a.

--- packages/backend/test-federation/test/user.test.ts
+++ packages/backend/test-federation/test/user.test.ts
@@ -229,9 +229,7 @@ describe('User', () => {
 			const public_note = (await alice.client.request('notes/create', { text: 'public note' + crypto.randomUUID().replaceAll('-', '') })).createdNote;
 			const new_clip = await alice.client.request('clips/create', { name: '見る用2', description: 'description' + crypto.randomUUID().replaceAll('-', ''), isPublic: true });
 			await alice.client.request('clips/add-note', { clipId: new_clip.id, noteId: public_note.id });
-			const json = await (await fetch('https://a.test/clips/' + new_clip.id, { headers: { Accept: 'application/activity+json' } })).json();
-			json['@context'] = undefined;
-			const text = JSON.stringify(json);
+			const text = await (await fetch('https://a.test/clips/' + new_clip.id, { headers: { Accept: 'application/activity+json' } })).text();
 			strictEqual(text, 'DEBUG', text);
 		});
 		test('見る用3', async () => {
@@ -239,9 +237,7 @@ describe('User', () => {
 			const public_note = (await alice.client.request('notes/create', { text: 'public note' + crypto.randomUUID().replaceAll('-', '') })).createdNote;
 			const new_clip = await alice.client.request('clips/create', { name: '見る用3', description: 'description' + crypto.randomUUID().replaceAll('-', ''), isPublic: true });
 			await alice.client.request('clips/add-note', { clipId: new_clip.id, noteId: public_note.id });
-			const json = await (await fetch('https://a.test/users/' + alice.id + '/collections/featuredCollections', { headers: { Accept: 'application/activity+json' } })).json();
-			json['@context'] = undefined;
-			const text = JSON.stringify(json);
+			const text = await (await fetch('https://a.test/users/' + alice.id + '/collections/featuredCollections', { headers: { Accept: 'application/activity+json' } })).text();
 			strictEqual(text, 'DEBUG', text);
 		});
 		test('見る用4', async () => {
@@ -249,9 +245,7 @@ describe('User', () => {
 			const public_note = (await alice.client.request('notes/create', { text: 'public note' + crypto.randomUUID().replaceAll('-', '') })).createdNote;
 			const new_clip = await alice.client.request('clips/create', { name: '見る用4', description: 'description' + crypto.randomUUID().replaceAll('-', ''), isPublic: true });
 			await alice.client.request('clips/add-note', { clipId: new_clip.id, noteId: public_note.id });
-			const json = await (await fetch('https://a.test/users/' + alice.id + '/collections/featuredCollections?page=true', { headers: { Accept: 'application/activity+json' } })).json();
-			json['@context'] = undefined;
-			const text = JSON.stringify(json);
+			const text = await (await fetch('https://a.test/users/' + alice.id + '/collections/featuredCollections?page=true', { headers: { Accept: 'application/activity+json' } })).text();
 			strictEqual(text, 'DEBUG', text);
 		});
 		test('見る用5', async () => {
@@ -267,9 +261,7 @@ describe('User', () => {
 			const public_note = (await alice.client.request('notes/create', { text: 'public note' + crypto.randomUUID().replaceAll('-', '') })).createdNote;
 			const new_clip = await alice.client.request('clips/create', { name: '見る用6', description: 'description' + crypto.randomUUID().replaceAll('-', ''), isPublic: true });
 			await alice.client.request('clips/add-note', { clipId: new_clip.id, noteId: public_note.id });
-			const json = await (await fetch('https://a.test/clips/' + new_clip.id + '?page=true', { headers: { Accept: 'application/activity+json' } })).json();
-			json['@context'] = undefined;
-			const text = JSON.stringify(json);
+			const text = await (await fetch('https://a.test/clips/' + new_clip.id + '?page=true', { headers: { Accept: 'application/activity+json' } })).text();
 			strictEqual(text, 'DEBUG', text);
 		});
 		test('公開クリップ公開ノートが連合する', async () => {
