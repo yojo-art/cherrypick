4ster1sk/misskey

アンテナ受信時に新しい投稿として通知できるように

packages/backend/src/core/AntennaService.ts
@@ -9,6 +9,7 @@ import { In } from 'typeorm';
 import { FanoutTimelineService } from '@/core/FanoutTimelineService.js';
 import type { GlobalEvents } from '@/core/GlobalEventService.js';
 import { GlobalEventService } from '@/core/GlobalEventService.js';
+import { NotificationService } from '@/core/NotificationService.js';
 import { UtilityService } from '@/core/UtilityService.js';
 import { bindThis } from '@/decorators.js';
 import { DI } from '@/di-symbols.js';
@@ -45,6 +46,7 @@ export class AntennaService implements OnApplicationShutdown {
 		private globalEventService: GlobalEventService,
 		private fanoutTimelineService: FanoutTimelineService,
 		private roleService: RoleService,
+		private notificationService: NotificationService,
 	) {
 		this.antennasFetched = false;
 		this.antennas = [];
@@ -113,6 +115,13 @@ export class AntennaService implements OnApplicationShutdown {
 
 			this.fanoutTimelineService.push(`antennaTimeline:${antenna.id}`, note.id, antennaNotesLimit, redisPipeline);
 			this.globalEventService.publishAntennaStream(antenna.id, 'note', note);
+		}
+
+		// notifyが有効なのが存在する場合は通知を送る
+		if (matchedAntennas.some(a => a.notify)) {
+			this.notificationService.createNotification(noteUser.id, 'note', {
+				noteId: note.id,
+			});
 		}
 
 		redisPipeline.exec();

yojo-art/cherrypick

packages/backend/src/core/AntennaService.ts
@@ -9,6 +9,7 @@ import { In } from 'typeorm';
 import { FanoutTimelineService } from '@/core/FanoutTimelineService.js';
 import type { GlobalEvents } from '@/core/GlobalEventService.js';
 import { GlobalEventService } from '@/core/GlobalEventService.js';
+import { NotificationService } from '@/core/NotificationService.js';
 import { UtilityService } from '@/core/UtilityService.js';
 import { bindThis } from '@/decorators.js';
 import { DI } from '@/di-symbols.js';
@@ -47,6 +48,7 @@ export class AntennaService implements OnApplicationShutdown {
 		private utilityService: UtilityService,
 		private globalEventService: GlobalEventService,
 		private fanoutTimelineService: FanoutTimelineService,
+		private notificationService: NotificationService,
 	) {
 		this.antennasFetched = false;
 		this.antennas = [];
@@ -90,9 +92,20 @@ export class AntennaService implements OnApplicationShutdown {
 
 		const redisPipeline = this.redisForTimelines.pipeline();
 
+		const notifiedUserIds = new Set<string>();
+
 		for (const antenna of matchedAntennas) {
 			this.fanoutTimelineService.push(`antennaTimeline:${antenna.id}`, note.id, 200, redisPipeline);
 			this.globalEventService.publishAntennaStream(antenna.id, 'note', note);
+
+			if (antenna.notify) {
+				if (!notifiedUserIds.has(antenna.userId)) {
+					this.notificationService.createNotification(antenna.userId, 'note', {
+						noteId: note.id,
+					});
+					notifiedUserIds.add(antenna.userId);
+				}
+			}
 		}
 
 		redisPipeline.exec();