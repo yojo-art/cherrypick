d0424a431b8d3dc70d702cc7408f8f1a1ca18cb0
pen <121443048+penginn-net@users.noreply.github.com>
2026-03-04 11:38:51 +0000

    importなど

--- packages/backend/src/queue/processors/OpenSearchIndexProcessorService.ts
+++ packages/backend/src/queue/processors/OpenSearchIndexProcessorService.ts
@@ -5,17 +5,14 @@
 
 import * as fs from 'node:fs';
 import { Inject, Injectable } from '@nestjs/common';
-import { ZipReader } from 'slacc';
-import { IsNull } from 'typeorm';
-import type { NotesRepository } from '@/models/_.js';
+import { Brackets } from 'typeorm';
+import * as Bull from 'bullmq';
+import type { NotesRepository, PollsRepository } from '@/models/_.js';
 import type Logger from '@/logger.js';
 import { AdvancedSearchService } from '@/core/AdvancedSearchService.js';
-import { createTempDir } from '@/misc/create-temp.js';
-import { DriveService } from '@/core/DriveService.js';
-import { DownloadService } from '@/core/DownloadService.js';
 import { bindThis } from '@/decorators.js';
+import { DI } from '@/di-symbols.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
-import * as Bull from 'bullmq';
 import type { OpenSerchIndexJobData } from '../types.js';
 
 // TODO: 名前衝突時の動作を選べるようにする
@@ -24,8 +21,10 @@ export class OpenSearchIndexProcessorService {
 	private logger: Logger;
 
 	constructor(
-		@Inject(DI.NotesRepository)
+		@Inject(DI.notesRepository)
 		private notesRepository: NotesRepository,
+		@Inject(DI.pollsRepository)
+		private pollsRepository: PollsRepository,
 
 		private advancedSearchService: AdvancedSearchService,
 		private queueLoggerService: QueueLoggerService,
@@ -37,26 +36,29 @@ export class OpenSearchIndexProcessorService {
 	public async process(job: Bull.Job<OpenSerchIndexJobData>): Promise<void> {
 		switch (job.data.type) {
 			case 'note':
+			{
 				const note = await this.notesRepository
-							.createQueryBuilder('note')
-							.leftJoin('note.renote', 'renote')
-							.select(['note'])
-							.where('note.id > :untilId', { untilId })
-							.andWhere(new Brackets( qb => {
-								qb.where('note."userHost" IS NULL')
-									.orWhere(new Brackets(qb2 => {
-										qb2
-											.where('note."userHost" IS NOT NULL')
-											.andWhere('note."searchableBy" != \'private\'')
-											.orWhere('note."searchableBy" IS NULL');
-									}));
-							}))
-							.orderBy('note.id', 'ASC');
-				if(note.hasPoll) {
+					.createQueryBuilder('note')
+					.leftJoin('note.renote', 'renote')
+					.select(['note'])
+					.where('note.id > :targetId', { targetId: job.data.untilId })
+					.andWhere(new Brackets( qb => {
+						qb.where('note."userHost" IS NULL')
+							.orWhere(new Brackets(qb2 => {
+								qb2
+									.where('note."userHost" IS NOT NULL')
+									.andWhere('note."searchableBy" != \'private\'')
+									.orWhere('note."searchableBy" IS NULL');
+							}));
+					}))
+					.orderBy('note.id', 'ASC')
+					.getOne();
+				if (!note) return;
+				if (note.hasPoll) {
 					const poll = await this.notesRepository.manager.getRepository('poll').findOneBy({ noteId: note.id });
-					await this.advancedSearchService.indexNote(note, poll ? note.poll.choices : undefined);
+					await this.advancedSearchService.indexNote(note, poll ? poll.choices : undefined);
 				} else await this.pollsRepository.findOneBy({ noteId: note.id });
-				break;
+				break; }
 
 			case 'noteAll':
 				await this.advancedSearchService.bulkIndexNote(job.data.untilId, job.data.limitId);
@@ -64,6 +66,5 @@ export class OpenSearchIndexProcessorService {
 			default:
 				throw new Bull.UnrecoverableError(`Unknown import type: ${job.data.type}`);
 		}
-
 	}
 }
