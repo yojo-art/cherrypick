0d758d11508c49c790c188e477b39e2be01d1fc8
4ster1sk <146138447+4ster1sk@users.noreply.github.com>
2026-03-21 20:18:42 +0900

    fix(frontend): リアクション確認ダイアログが正しく動作していないのを修正 (#895)

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -666,7 +666,8 @@ provide(DI.mfmEmojiReactCallback, (reaction) => {
 	notesReactionsCreate({
 		noteId: appearNote.id,
 		reaction: reaction,
-	}).then(() => {
+	}).then(({ canceled }) => {
+		if (canceled) return;
 		noteEvents.emit(`reacted:${appearNote.id}`, {
 			userId: $i!.id,
 			reaction: reaction,
@@ -815,7 +816,8 @@ function react(): void {
 		notesReactionsCreate({
 			noteId: appearNote.id,
 			reaction: '❤️',
-		}).then(() => {
+		}).then(({ canceled }) => {
+			if (canceled) return;
 			noteEvents.emit(`reacted:${appearNote.id}`, {
 				userId: $i!.id,
 				reaction: '❤️',
@@ -871,10 +873,20 @@ async function toggleReaction(reaction) {
 		misskeyApi('notes/reactions/delete', {
 			noteId: note.id,
 		}).then(() => {
+			noteEvents.emit(`unreacted:${appearNote.id}`, {
+				userId: $i!.id,
+				reaction: oldReaction,
+			});
+
 			if (oldReaction !== reaction) {
 				misskeyApi('notes/reactions/create', {
 					noteId: note.id,
 					reaction: reaction,
+				}).then(() => {
+					noteEvents.emit(`reacted:${appearNote.id}`, {
+						userId: $i!.id,
+						reaction: reaction,
+					});
 				});
 			}
 		});
@@ -882,7 +894,8 @@ async function toggleReaction(reaction) {
 		notesReactionsCreate({
 			noteId: appearNote.id,
 			reaction: reaction,
-		}).then(() => {
+		}).then(({ canceled }) => {
+			if (canceled) return;
 			noteEvents.emit(`reacted:${appearNote.id}`, {
 				userId: $i!.id,
 				reaction: reaction,
@@ -907,21 +920,28 @@ function heartReact(): void {
 	notesReactionsCreate({
 		noteId: appearNote.id,
 		reaction: prefer.s.selectReaction,
-	});
+	}).then(({ canceled }) => {
+		if (canceled) return;
 
-	if (appearNote.text && appearNote.text.length > 100 && (Date.now() - new Date(appearNote.createdAt).getTime() < 1000 * 3)) {
-		claimAchievement('reactWithoutRead');
-	}
-
-	const el = heartReactButton.value;
-	if (el && prefer.s.animation) {
-		const rect = el.getBoundingClientRect();
-		const x = rect.left + (el.offsetWidth / 2);
-		const y = rect.top + (el.offsetHeight / 2);
-		const { dispose } = os.popup(MkRippleEffect, { x, y }, {
-			end: () => dispose(),
+		noteEvents.emit(`reacted:${props.note.id}`, {
+			userId: $i!.id,
+			reaction: prefer.s.selectReaction,
 		});
-	}
+
+		if (appearNote.text && appearNote.text.length > 100 && (Date.now() - new Date(appearNote.createdAt).getTime() < 1000 * 3)) {
+			claimAchievement('reactWithoutRead');
+		}
+
+		const el = heartReactButton.value;
+		if (el && prefer.s.animation) {
+			const rect = el.getBoundingClientRect();
+			const x = rect.left + (el.offsetWidth / 2);
+			const y = rect.top + (el.offsetHeight / 2);
+			const { dispose } = os.popup(MkRippleEffect, { x, y }, {
+				end: () => dispose(),
+			});
+		}
+	});
 }
 
 function undoReact(): void {
