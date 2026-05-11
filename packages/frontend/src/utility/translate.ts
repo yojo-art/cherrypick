d661794f0fa36d5e8a7e6de8d4cd0a74405a904e
4sterisk <4sterisk@ymail.ne.jp>
2026-05-11 14:55:52 +0000

    fix

--- packages/frontend/src/utility/translate.ts
+++ packages/frontend/src/utility/translate.ts
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 const STATUSES = ['none', 'running', 'success', 'error'] as const;
 export type TranslateStatus = typeof STATUSES[number];
