db5347f19fbb7061eb96f86f39fdfd1a02f5a784
4sterisk <4sterisk@ymail.ne.jp>
2026-05-23 02:59:31 +0000

    fix(test): misskey-devに変更してしまっていたのを修正

--- packages/backend/test/e2e/drive.ts
+++ packages/backend/test/e2e/drive.ts
@@ -23,7 +23,7 @@ describe('Drive', () => {
 
 		const marker = Math.random().toString();
 
-		const url = 'https://raw.githubusercontent.com/misskey-dev/misskey/develop/packages/backend/test/resources/192.jpg';
+		const url = 'https://raw.githubusercontent.com/yojo-art/cherrypick/develop/packages/backend/test/resources/192.jpg';
 
 		const catcher = makeStreamCatcher(
 			alice,
