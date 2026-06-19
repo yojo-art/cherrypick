568c7bff8b9f6294e3f006fa7c5575f357bd13a4
4sterisk <4sterisk@ymail.ne.jp>
2026-06-20 01:40:15 +0900

    generate_stubでactor鍵を生成しないように

--- packages/backend/test-federation/setup.sh
+++ packages/backend/test-federation/setup.sh
@@ -46,7 +46,6 @@ function generate_stub {
     -out certificates/$1.crt \
     -days 500
   sed "s/\${HOST}/$1/g" .config/example.stub.conf > .config/$1.conf
-  node stub/generate-actor-keys.mjs stub
 }
 
 generate a.test
