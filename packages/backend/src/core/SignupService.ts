16d02a4900df2546390a898a5bada2e74ba5d0bd
4ster1sk <146138447+4ster1sk@users.noreply.github.com>
2026-07-19 13:14:53 +0900

    fix: チャンネル作成時にバナーが設定されない問題を修正 (#1197)

--- packages/backend/src/core/SignupService.ts
+++ packages/backend/src/core/SignupService.ts
@@ -11,7 +11,8 @@ import * as argon2 from 'argon2';
 import { DataSource, IsNull } from 'typeorm';
 import { DriveFile } from 'misskey-js/entities.js';
 import { DI } from '@/di-symbols.js';
-import type { ChannelsRepository, MiMeta, UsedUsernamesRepository, UsersRepository } from '@/models/_.js';
+import { DriveFileEntityService } from '@/core/entities/DriveFileEntityService.js';
+import type { ChannelsRepository, DriveFilesRepository, MiMeta, UsedUsernamesRepository, UsersRepository } from '@/models/_.js';
 import { MiUser } from '@/models/User.js';
 import { MiChannel } from '@/models/Channel.js';
 import { MiUserProfile } from '@/models/UserProfile.js';
@@ -44,6 +45,9 @@ export class SignupService {
 		@Inject(DI.channelsRepository)
 		private channelsRepository: ChannelsRepository,
 
+		@Inject(DI.driveFilesRepository)
+		private driveFilesRepository: DriveFilesRepository,
+
 		@Inject(DI.usedUsernamesRepository)
 		private usedUsernamesRepository: UsedUsernamesRepository,
 
@@ -54,6 +58,7 @@ export class SignupService {
 		private systemAccountService: SystemAccountService,
 		private metaService: MetaService,
 		private usersChart: UsersChart,
+		private driveFileEntityService: DriveFileEntityService,
 	) {
 	}
 
@@ -248,6 +253,10 @@ export class SignupService {
 		}
 		let account!: MiUser;
 		let channel!:MiChannel;
+		let bannerFile = null;
+		if (bannerId != null) {
+			bannerFile = await this.driveFilesRepository.findOneBy({ id: bannerId });
+		}
 
 		// Start transaction
 		await this.db.transaction(async transactionalEntityManager => {
@@ -268,6 +277,8 @@ export class SignupService {
 				tags,
 				token: secret,
 				bannerId: bannerId ?? null,
+				bannerUrl: bannerFile ? this.driveFileEntityService.getPublicUrl(bannerFile) : null,
+				bannerBlurhash: bannerFile ? bannerFile.blurhash : null,
 			}));
 
 			await transactionalEntityManager.save(new MiUserKeypair({
@@ -294,6 +305,7 @@ export class SignupService {
 				actor: account,
 				actorId: account.id,
 				description,
+				bannerId: bannerId ?? null,
 			}));
 			await transactionalEntityManager.update(MiUser, {
 				id: account.id,
--- packages/backend/test/e2e/channel.ts
+++ packages/backend/test/e2e/channel.ts
@@ -6,7 +6,7 @@
 process.env.NODE_ENV = 'test';
 
 import * as assert from 'assert';
-import { api, randomString, signup } from '../utils.js';
+import { api, randomString, signup, uploadUrl } from '../utils.js';
 import type * as misskey from 'misskey-js';
 
 describe('channels/create', () => {
@@ -104,6 +104,18 @@ describe('channels/create', () => {
 	});
 
 	describe('チャンネル作成時の基本設定', () => {
+		test('チャンネル作成時にバナーが設定される', async () => {
+			const file = await uploadUrl(root, 'https://raw.githubusercontent.com/yojo-art/cherrypick/develop/packages/backend/test/resources/192.jpg');
+			const username = randomString();
+			const name = randomString() + ' Channel';
+			const ch = await api('channels/create', { username: username, name: name, bannerId: file.id }, root);
+			assert.strictEqual(ch.status, 200);
+			assert.notStrictEqual(ch.body.bannerUrl, null, 'チャンネルのbannerUrlが設定される');
+			const channelActor = await api('users/show', { userId: ch.body.actorId! }, root);
+			assert.notStrictEqual(channelActor.body.bannerUrl, null, 'チャンネルアカウントのbannerUrlが設定される');
+			assert.notStrictEqual(channelActor.body.bannerBlurhash, null, 'チャンネルアカウントのbannerBlurhashが設定される');
+		});
+
 		test('チャンネル作成時にユーザーの名前が設定される', async () => {
 			const username = randomString();
 			const name = randomString() + ' Channel';
